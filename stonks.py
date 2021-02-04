fdbc32255c246a028fa15ff0b1915c9dde0864d2
scheung <scheung97@DESKTOP-MLG4DJ9.localdomain>
2021-02-03 23:37:03 -0500

    dash

--- stonks.py
+++ stonks.py
@@ -11,9 +11,12 @@ totp = pyotp.TOTP("My2factorAppHere").now()
 robin_stocks.authentication.login(username=config.username, password=config.password, store_session=True, mfa_code=totp )
 #r = robin_stocks.helper.request_get("https://api.robinhood.com/midlands/tags/tag/technology/", jsonify_data=True)
 r = robin_stocks.build_holdings() #builds dictionary regard stocks+positions the owner has
-print("=========================================================================")
+
+dash = "="*73
+print(dash)
 print("Stonk:  Curr_price:\t     Shares Owned:")
-print("=========================================================================")
+print(dash)
+
 for key,val in r.items(): #key = ticker symbol, val = json_data
     print("{:<5s}\t  {:>6.2f}\t\t{:>3.4f}".format(key, float(val['price']), float(val['quantity'])))
 robin_stocks.authentication.logout()
