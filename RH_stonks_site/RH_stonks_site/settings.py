83f2c75e142f325d410b9f19b6999ddc4db0f08f
scheung97 <scheung1997@gmail.com>
2021-02-28 23:09:59 -0500

    remove secret key

--- RH_stonks_site/RH_stonks_site/settings.py
+++ RH_stonks_site/RH_stonks_site/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'et-mhax1zpugq^#_wq-zh$y%#!lv1p=##jw(p*^ij#^uk+xtcg'
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
